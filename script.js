b6b0af1f820ccd18ab14a5478f06a3702e8feec5
HarshalWarade <92624545+HarshalWarade@users.noreply.github.com>
2022-04-17 01:09:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
+
+
+
 let toggles = document.getElementsByClassName('toggle');
 let contentDiv = document.getElementsByClassName('content__last');
 let icons = document.getElementsByClassName('icon__last');
 for (let i = 0; i < toggles.length; i++) {
     toggles[i].addEventListener('click', () => {
-        if (parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight) {
+        if(parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight){
             contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
             toggles[i].style.color = "#ff0";
-            // toggles[i].style.fontWeight = "700";
-            toggles.style.boxshadow = "box-shadow: 0 0 5px red;";
             icons[i].classList.remove('fa-plus');
             icons[i].classList.add('fa-minus');
         }
-        else {
+        else{
             contentDiv[i].style.height = "0px";
-            toggles[i].style.color = "#fff";
+            toggles[i].style.color = "#ffffff";
             icons[i].classList.remove('fa-minus');
             icons[i].classList.add('fa-plus');
         }
 
-        for (let j = 0; j < contentDiv.length; j++) {
-            if (j !== i) {
+        for(let j=0; j<contentDiv.length; j++){
+            if(j!==i){
                 contentDiv[j].style.height = "0px";
                 toggles[j].style.color = "#ffffff";
                 icons[j].classList.remove('fa-minus');
@@ -28,3 +29,5 @@ for (let i = 0; i < toggles.length; i++) {
         }
     });
 };
+
+
